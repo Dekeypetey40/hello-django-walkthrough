cef31900ded77afc1068b587781835715086100b
Kai <kmichaelmikhail@gmail.com>
2023-08-23 13:45:09 +0200

    Add titltes to html files and environment variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-81@2b#l$qz5vgfk*lpy75b!ij1^bl@h5+!sic*xr-3t6y3)729'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-81@2b#l$qz5vgfk*lpy75b!ij1^bl@h5+!sic*xr-3t6y3)729')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kais-django-task-manager-566444d0e63d.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
